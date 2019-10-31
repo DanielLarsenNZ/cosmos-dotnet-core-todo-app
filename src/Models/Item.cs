694e1c8377482cbd12e03b3d7be299eb5ed138e4
Daniel Larsen <dalars@microsoft.com>
2019-10-31 13:46:23 +1300

    Upgrade to ASP.NET 3.0, remove dependency on Newtonsoft

--- src/Models/Item.cs
+++ src/Models/Item.cs
@@ -1,19 +1,21 @@
-﻿namespace todo.Models
-{
-    using Newtonsoft.Json;
+﻿using System.Text.Json.Serialization;
+//using Newtonsoft.Json;
 
+namespace todo.Models
+{
     public class Item
     {
-        [JsonProperty(PropertyName = "id")]
+        //[JsonProperty(PropertyName = "id")]
+        [JsonPropertyName("id")]
         public string Id { get; set; }
 
-        [JsonProperty(PropertyName = "name")]
+        //[JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
 
-        [JsonProperty(PropertyName = "description")]
+        //[JsonProperty(PropertyName = "description")]
         public string Description { get; set; }
 
-        [JsonProperty(PropertyName = "isComplete")]
+        //[JsonProperty(PropertyName = "isComplete")]
         public bool Completed { get; set; }
     }
 }
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,17 +1,18 @@
-﻿namespace todo
-{
-    using System.Threading.Tasks;
-    using todo.Models;
-    using todo.Services;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Azure.Cosmos;
-    using Microsoft.Azure.Cosmos.Fluent;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
+﻿using System.Threading.Tasks;
+using todo.Models;
+using todo.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
+namespace todo
+{
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -31,13 +32,13 @@
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options=> options.EnableEndpointRouting = false)
+            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddSingleton<ICosmosDbService>(InitializeCosmosClientInstanceAsync(Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
